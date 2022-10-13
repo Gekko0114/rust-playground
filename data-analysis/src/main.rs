09062e8c8bcc4a188b1b9330783bb6c0edcf95eb
moriya <moriya0114.pharmacy@gmail.com>
2022-10-13 22:52:19 +0900

    simple_nn

--- data-analysis/src/main.rs
+++ data-analysis/src/main.rs
@@ -1,9 +1,21 @@
 use std::process::exit;
+use std::env::args;
 
 mod linear_with_sgd;
-
+mod simple_nn;
 fn main() {
-    let res = linear_with_sgd::run();
+    let args: Vec<String> = args().collect();
+    let model = if args.len() < 2 {
+        None
+    } else {
+        Some(args[1].as_str())
+    };
+
+    let res = match model {
+        None => {println!("Run cargo run [nn|linear sdg] to get outputs", ); Ok(())},
+        Some("nn") => simple_nn::run(),
+        Some(_) => linear_with_sgd::run(),
+    };
 
     exit(match res {
         Ok(_) => 0,
