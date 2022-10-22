9f563cd0ca7772e94b8ea817eb91755e74feaf7a
moriya <moriya0114.pharmacy@gmail.com>
2022-10-22 14:14:37 +0900

    chapter11

--- tdd/src/lib.rs
+++ tdd/src/lib.rs
@@ -7,21 +7,13 @@ pub fn test_multiplication() {
     use super::*;
     let five = money::Money::dollar(5);
     assert!(money::Money::dollar(10).equals(five.times(2)));
-    assert!(money::Money::dollar(15).equals(five.times(3)));
 }
 #[test]
 pub fn test_equality() {
     use super::*;
     assert!(money::Money::dollar(5).equals(money::Money::dollar(5)));
+    assert!(!money::Money::dollar(5).equals(money::Money::dollar(1)));
     assert!(!money::Money::dollar(5).equals(money::Money::franc(5)));
-}
-
-#[test]
-pub fn test_flanc_multiplication() {
-    use super::*;
-    let five = money::Money::franc(5);
-    assert!(money::Money::franc(10).equals(five.times(2)));
-    assert!(money::Money::franc(15).equals(five.times(3)));
 }
 
 #[test]
