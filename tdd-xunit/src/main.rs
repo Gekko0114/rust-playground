7466fb25c27e9f7976357e7404934a022fae34ba
moriya <moriya0114.pharmacy@gmail.com>
2022-10-30 15:06:42 +0900

    chapter22

--- tdd-xunit/src/main.rs
+++ tdd-xunit/src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     let mut test_case_test=  TestCaseTest{test: Box::new(WasRun{..Default::default()})};
     test_case_test.test_template_method();
     test_case_test.test_result();
+    test_case_test.test_failed_result_formatting();
+    test_case_test.test_failed_result();
 }
--- tdd-xunit/src/test_libs.rs
+++ tdd-xunit/src/test_libs.rs
@@ -4,17 +4,21 @@ pub struct WasRun {
 }
 
 impl WasRun {
-    pub fn test_method(&mut self) {
+    pub fn test_method(&mut self) -> Result<usize, &'static str> {
         self.log = self.log.clone() + "testMethod ";
+        Ok(1)
+
     }
 
-    pub fn test_broken_method(&mut self) {
-        panic!("error");
+    pub fn test_broken_method(&mut self) -> Result<usize, &'static str> {
+        Err("error")
     }
 }
 
+#[derive(Default)]
 pub struct TestResult {
-    pub run_count: i32    
+    pub run_count: i32,
+    pub error_count: i32
 }
 
 impl TestResult {
@@ -22,8 +26,12 @@ impl TestResult {
         self.run_count += 1;
     }
 
+    pub fn test_failed(&mut self) {
+        self.error_count += 1;
+    }
+
     pub fn summary(&self) -> String {
-        self.run_count.to_string() + " run, 0 failed"
+        self.run_count.to_string() + " run, " + &self.error_count.to_string() + " failed"
     }
 }
 
@@ -36,13 +44,20 @@ pub trait TestCaseTrait {
 
 impl TestCaseTrait for WasRun {
     fn run(&mut self, func_name: &str) -> TestResult {
-        let mut result = TestResult{run_count: 0};
+        let mut result: TestResult = Default::default();
         result.test_started();
         self.set_up();
-        match func_name {
+        let f = match func_name {
             "test_method" => self.test_method(),
             "test_broken_method" => self.test_broken_method(),
-            _ => println!("no match")
+            _ => Err("no match functions")
+        };        
+        let _value = match f {
+            Ok(_v) => 1,
+            Err(_e) => {
+                result.test_failed();
+                0
+            }
         };
         self.tear_down();
         result
@@ -73,11 +88,21 @@ impl TestCaseTest {
 
     pub fn test_result(&mut self) {
         let result = self.test.run("test_method");
+        println!("{}", result.summary());        
         assert!("1 run, 0 failed" == result.summary());
     }
 
     pub fn test_failed_result(&mut self) {
         let result = self.test.run("test_broken_method");
+        println!("{}", result.summary());
+        assert!("1 run, 1 failed" == result.summary());
+    }
+
+    pub fn test_failed_result_formatting(&mut self) {
+        let mut result: TestResult = Default::default();
+        result.test_started();
+        result.test_failed();
+        println!("{}", result.summary());        
         assert!("1 run, 1 failed" == result.summary());
     }
 }
